267577610b537bba5073e6e728ebcdfcd8a88ef0
Takamitsu Endo <ryukau@gmail.com>
2022-09-28 20:00:46 +0900

    Add PadCymbal2

---
+++ PadCymbal2/main.js
@@ -0,0 +1,204 @@
+// Copyright 2022 Takamitsu Endo
+// SPDX-License-Identifier: Apache-2.0
+
+import {uiSize} from "../common/gui/palette.js";
+import * as widget from "../common/gui/widget.js";
+import * as parameter from "../common/parameter.js";
+import * as util from "../common/util.js";
+import * as wave from "../common/wave.js";
+
+import * as menuitems from "./menuitems.js";
+
+function randomize() {
+  // console.log(selectRandom.value);
+
+  for (const key in param) {
+    if (key === "renderDuration") continue;
+    if (key === "matrixSize") continue;
+    if (key === "fadeOut") continue;
+    if (key === "attackSecond") continue;
+    if (key === "stereoRandom") continue;
+    if (key === "padMix") continue;
+    if (key === "lowpassHz") continue;
+    if (key === "lowpassQ") continue;
+    if (key === "highpassHz") continue;
+    if (key === "highpassQ") continue;
+
+    if (Array.isArray(param[key])) {
+      param[key].forEach(e => { e.normalized = Math.random(); });
+    } else if (param[key].scale instanceof parameter.MenuItemScale) {
+      // Do nothing for now.
+    } else {
+      param[key].normalized = Math.random();
+    }
+  }
+
+  render();
+  widget.refresh(ui);
+}
+
+function createArrayParameters(defaultDspValue, scale) {
+  let arr = new Array(scales.matrixSize.max);
+  for (let i = 0; i < arr.length; ++i) {
+    arr[i] = new parameter.Parameter(defaultDspValue, scale, true);
+  }
+  return arr;
+}
+
+function render() {
+  audio.render(
+    parameter.toMessage(param, {
+      sampleRate: audio.audioContext.sampleRate,
+    }),
+    "perChannel",
+    togglebuttonQuickSave.state === 1,
+  );
+}
+
+const scales = {
+  defaultScale: new parameter.LinearScale(0, 1),
+
+  renderDuration: new parameter.DecibelScale(-40, 40, false),
+  fade: new parameter.DecibelScale(-60, 40, true),
+  overSample: new parameter.MenuItemScale(menuitems.oversampleItems),
+
+  nLayer: new parameter.IntScale(1, 8),
+  interval: new parameter.DecibelScale(-60, -20, true),
+  seed: new parameter.IntScale(0, 2 ** 53),
+
+  decayTo: new parameter.DecibelScale(util.ampToDB(1 / 2 ** 24), 0, false),
+  attackSecond: new parameter.DecibelScale(-60, -20, true),
+  minFreq: new parameter.MidiPitchScale(
+    util.freqToMidiPitch(20), util.freqToMidiPitch(16000), false),
+  numBin: new parameter.DecibelScale(20, 60, false),
+  bandWidthOctave: new parameter.DecibelScale(-60, 0, false),
+  gainSlope: new parameter.DecibelScale(-6, 6, false),
+
+  combSection: new parameter.MenuItemScale(menuitems.combSectionItems),
+  padMix: new parameter.DecibelScale(-80, 20, true),
+  nDelay: new parameter.IntScale(1, 32),
+  delayTime: new parameter.DecibelScale(-60, util.ampToDB(0.2), true),
+  feedback: new parameter.LinearScale(-1, 1),
+  highpassHz: new parameter.MidiPitchScale(
+    util.freqToMidiPitch(1), util.freqToMidiPitch(48000), true),
+  lowpassHz: new parameter.MidiPitchScale(
+    util.freqToMidiPitch(10), util.freqToMidiPitch(48000), false),
+  filterQ: new parameter.LinearScale(0.1, Math.SQRT1_2),
+};
+
+const param = {
+  renderDuration: new parameter.Parameter(1 / 3, scales.renderDuration, true),
+  fadeOut: new parameter.Parameter(0.002, scales.fade, true),
+  overSample: new parameter.Parameter(1, scales.overSample),
+
+  nLayer: new parameter.Parameter(3, scales.nLayer),
+  interval: new parameter.Parameter(0.02, scales.interval, true),
+  jitter: new parameter.Parameter(0.1, scales.defaultScale),
+  seed: new parameter.Parameter(0, scales.seed),
+
+  decayTo: new parameter.Parameter(0.01, scales.decayTo, false),
+  attackSecond: new parameter.Parameter(0, scales.attackSecond, true),
+  minFreq: new parameter.Parameter(80, scales.minFreq, true),
+  numBin: new parameter.Parameter(1000, scales.numBin, true),
+  bandWidthOctave: new parameter.Parameter(0.01, scales.bandWidthOctave, true),
+  gainSlope: new parameter.Parameter(1, scales.gainSlope, false),
+  stereoRandom: new parameter.Parameter(0, scales.defaultScale),
+
+  combSection: new parameter.Parameter(
+    menuitems.combSectionItems.indexOf("Active"), scales.combSection),
+  padMix: new parameter.Parameter(1, scales.padMix),
+  nDelay: new parameter.Parameter(16, scales.nDelay),
+  delayTime: new parameter.Parameter(0.03, scales.delayTime, true),
+  feedback: new parameter.Parameter(-0.98, scales.feedback),
+  lowpassHz: new parameter.Parameter(scales.lowpassHz.maxDsp, scales.lowpassHz, true),
+  lowpassQ: new parameter.Parameter(Math.SQRT1_2, scales.filterQ, true),
+  highpassHz: new parameter.Parameter(70, scales.highpassHz, true),
+  highpassQ: new parameter.Parameter(Math.SQRT1_2, scales.filterQ, true),
+};
+
+// Add controls.
+const audio = new wave.Audio(
+  2,
+  "./renderer.js",
+  undefined,
+  (wave) => {
+    for (let i = 0; i < waveView.length; ++i) waveView[i].set(wave.data[i]);
+  },
+);
+
+const pageTitle = widget.heading(document.body, 1, document.title, undefined, undefined);
+const divMain = widget.div(document.body, "main", undefined);
+
+const divLeft = widget.div(divMain, undefined, "controlBlock");
+const divRight = widget.div(divMain, undefined, "controlBlock");
+
+const headingWaveform = widget.heading(divLeft, 6, "Waveform");
+const waveView = [
+  new widget.WaveView(
+    divLeft, uiSize.waveViewWidth, uiSize.waveViewHeight, audio.wave.data[0], false),
+  new widget.WaveView(
+    divLeft, uiSize.waveViewWidth, uiSize.waveViewHeight, audio.wave.data[1], false),
+];
+
+const pRenderStatus = widget.paragraph(divLeft, "renderStatus", undefined);
+audio.renderStatusElement = pRenderStatus;
+
+const divPlayControl = widget.div(divLeft, "playControl", undefined);
+const selectRandom = widget.select(
+  divPlayControl, "Randomize Recipe", "randomRecipe", undefined, ["Default"], "Default",
+  (ev) => { randomize(); });
+const buttonRandom = widget.Button(divPlayControl, "Random", (ev) => { randomize(); });
+buttonRandom.id = "randomRecipe";
+const spanPlayControlFiller = widget.span(divPlayControl, "playControlFiller", undefined);
+const buttonPlay = widget.Button(divPlayControl, "Play", (ev) => { audio.play(); });
+const buttonStop = widget.Button(divPlayControl, "Stop", (ev) => { audio.stop(); });
+const buttonSave = widget.Button(divPlayControl, "Save", (ev) => { audio.save(); });
+const togglebuttonQuickSave = new widget.ToggleButton(
+  divPlayControl, "QuickSave", undefined, undefined, 0, (ev) => {});
+
+const detailRender = widget.details(divLeft, "Render");
+const detailLayer = widget.details(divLeft, "Layer");
+const detailPad = widget.details(divRight, "PADsynth");
+const detailComb = widget.details(divRight, "Serial Comb");
+
+const ui = {
+  renderDuration:
+    new widget.NumberInput(detailRender, "Duration [s]", param.renderDuration, render),
+  fadeOut: new widget.NumberInput(detailRender, "Fade-out [s]", param.fadeOut, render),
+  overSample:
+    new widget.ComboBoxLine(detailRender, "Over-sample", param.overSample, render),
+
+  nLayer: new widget.NumberInput(detailLayer, "nLayer", param.nLayer, render),
+  interval: new widget.NumberInput(detailLayer, "Interval [s]", param.interval, render),
+  jitter: new widget.NumberInput(detailLayer, "Jitter", param.jitter, render),
+  seed: new widget.NumberInput(detailLayer, "Seed", param.seed, render),
+
+  decayTo: new widget.NumberInput(detailPad, "Decay To [dB]", param.decayTo, render),
+  attackSecond:
+    new widget.NumberInput(detailPad, "Attack [s]", param.attackSecond, render),
+  minFreq:
+    new widget.NumberInput(detailPad, "Min. Frequency [Hz]", param.minFreq, render),
+  numBin: new widget.NumberInput(detailPad, "nBin", param.numBin, render),
+  bandWidthOctave:
+    new widget.NumberInput(detailPad, "Band Width [oct]", param.bandWidthOctave, render),
+  gainSlope:
+    new widget.NumberInput(detailPad, "Gain Slope [dB/oct]", param.gainSlope, render),
+  stereoRandom:
+    new widget.NumberInput(detailPad, "Stereo Randomize", param.stereoRandom, render),
+
+  combSection: new widget.ToggleButtonLine(
+    detailComb, menuitems.combSectionItems, param.combSection, render),
+  padMix: new widget.NumberInput(detailComb, "Input Mix [dB]", param.padMix, render),
+  nDelay: new widget.NumberInput(detailComb, "nDelay", param.nDelay, render),
+  delayTime:
+    new widget.NumberInput(detailComb, "Delay Time [s]", param.delayTime, render),
+  feedback: new widget.NumberInput(detailComb, "Feedback", param.feedback, render),
+  lowpassHz: new widget.NumberInput(detailComb, "LP Cut [Hz]", param.lowpassHz, render),
+  lowpassQ: new widget.NumberInput(detailComb, "LP Q", param.lowpassQ, render),
+  highpassHz: new widget.NumberInput(detailComb, "HP Cut [Hz]", param.highpassHz, render),
+  highpassQ: new widget.NumberInput(detailComb, "HP Q", param.highpassQ, render),
+};
+
+ui.feedback.number.step = 0.01;
+
+render();
---
+++ PadCymbal2/renderer.js
@@ -0,0 +1,163 @@
+// Copyright 2022 Takamitsu Endo
+// SPDX-License-Identifier: Apache-2.0
+
+import {Delay, IntDelay, LongAllpass} from "../common/dsp/delay.js";
+import * as multirate from "../common/dsp/multirate.js";
+import {SVF} from "../common/dsp/svf.js";
+import * as util from "../common/util.js";
+import {PcgRandom} from "../lib/pcgrandom/pcgrandom.js";
+import PocketFFT from "../lib/pocketfft/pocketfft.js";
+
+import * as menuitems from "./menuitems.js";
+
+// PadSynth from ZynAddSubFX
+// http://zynaddsubfx.sourceforge.net/doc/PADsynth/PADsynth.htm
+function padsynth(fft, sampleRate, sigSize, freq, gain, bandWidthOct, gainSlope, rng) {
+  sigSize += sigSize % 2;
+  const spcSize = Math.floor(sigSize / 2) + 1;
+
+  let spectrum = new fft.vector_complex128();
+  spectrum.resize(spcSize);
+
+  for (let idx = 0; idx < freq.length; ++idx) {
+    const bandWidthHz = (Math.pow(2, bandWidthOct) - 1) * freq[idx];
+    const bandWidthI = bandWidthHz / (2 * sampleRate);
+
+    const sigma = Math.sqrt(Math.pow(bandWidthI, 2) / Math.PI / 2);
+    const half = Math.max(Math.floor(6 * spcSize * sigma), 1);
+
+    const freqI = freq[idx] / sampleRate;
+
+    const mid = Math.floor(freqI * spcSize);
+    const start = Math.max(mid - half, 1); // Don't write index 0 to avoid DC.
+    const end = Math.min(mid + half, spcSize);
+
+    for (let j = start; j < end; ++j) {
+      const x = (j / spcSize - freqI) / bandWidthI;
+      const value = spectrum.getReal(j) + gain[idx] * Math.exp(-x * x) / bandWidthI;
+      spectrum.setReal(j, value);
+    }
+  }
+
+  // Randomize phase.
+  const nyquistHz = sampleRate / 2;
+  const nyquistOct = Math.log2(nyquistHz);
+  for (let idx = 1; idx < spcSize; ++idx) {
+    const oct = Math.log2(idx / (spcSize - 1) * nyquistHz);
+    const filterGain = gainSlope ** (nyquistOct - oct);
+
+    const real = spectrum.getReal(idx) * filterGain;
+    const theta = rng.number() * 2 * Math.PI;
+
+    spectrum.setValue(idx, real * Math.cos(theta), real * Math.sin(theta));
+  }
+
+  const output = fft.c2r(spectrum);
+  spectrum.delete();
+
+  let sound = new Array(output.size());
+  for (let i = 0; i < output.size(); ++i) sound[i] = output.get(i);
+  output.delete();
+
+  return sound;
+}
+
+function process(pv, dsp, input) {
+  let ap = input - pv.feedback * dsp.feedbackBuffer;
+  let output = dsp.feedbackBuffer + util.lerp(0, pv.feedback * input, pv.padMix);
+  for (let i = 0; i < pv.nDelay; ++i) {
+    ap = dsp.lowpass[i].lp(ap);
+    ap = dsp.highpass[i].hp(ap);
+    ap = 1000 * Math.tanh(0.001 * dsp.delay[i].process(ap));
+  }
+  dsp.feedbackBuffer = ap;
+  return output;
+}
+
+onmessage = async (event) => {
+  const fft = await PocketFFT();
+  const pv = event.data; // Parameter values.
+
+  const upFold = parseInt(menuitems.oversampleItems[pv.overSample]);
+  const upRate = upFold * pv.sampleRate;
+  const numBin = Math.floor(pv.numBin);
+  const minGain = 0.01;
+
+  let duration = Math.floor(upRate * pv.renderDuration);
+  duration += duration % 2;
+
+  const attackSamples = Math.max(1, Math.floor(upRate * pv.attackSecond));
+
+  let rng = new PcgRandom(BigInt(pv.seed));
+  let rngCh = new PcgRandom(BigInt(pv.seed + pv.channel * 65537));
+  const mixedRandom = () => util.lerp(rng.number(), rngCh.number(), pv.stereoRandom);
+
+  let buf = new Array(duration).fill(0);
+
+  // PADsynth.
+  let delay = 0;
+  const intervalSamples = pv.interval * upRate;
+  for (let layer = 0; layer < pv.nLayer; ++layer) {
+    let freq = new Array(numBin);
+    let gain = new Array(numBin);
+    for (var i = 0; i < numBin; ++i) {
+      freq[i] = pv.minFreq + mixedRandom() * (upRate / 2 - pv.minFreq)
+      gain[i] = minGain + mixedRandom() * (1 - minGain)
+    }
+    let pad = padsynth(
+      fft, upRate, duration, freq, gain, pv.bandWidthOctave, pv.gainSlope, rngCh);
+
+    // Post effect.
+    let gainEnv = 1;
+    let decay = Math.pow(pv.decayTo, 1.0 / (buf.length - delay));
+    const attackEnd = Math.min(buf.length, delay + attackSamples);
+    for (let i = delay; i < attackEnd; ++i) {
+      const attack
+        = Math.cos(Math.PI * 0.5 * (attackSamples - i + delay) / attackSamples);
+      buf[i] += pad[i] * gainEnv * attack;
+      gainEnv *= decay;
+    }
+    for (let i = attackEnd; i < buf.length; ++i) {
+      buf[i] += pad[i] * gainEnv;
+      gainEnv *= decay;
+    }
+
+    delay += Math.floor(util.lerp(
+      intervalSamples, -Math.log(1 - mixedRandom()) * intervalSamples, pv.jitter));
+  }
+
+  // Serial allpass.
+  let dsp = {
+    delay: [],
+    lowpass: [],
+    highpass: [],
+    feedbackBuffer: 0,
+  };
+
+  for (let i = 0; i < pv.nDelay; ++i) {
+    let timeInSeconds = (1 + rng.number()) * pv.delayTime / (i + 1);
+
+    let delay = new LongAllpass(upRate, timeInSeconds, upRate < 8 ? Delay : IntDelay);
+    delay.prepare(upRate * timeInSeconds, pv.feedback);
+    dsp.delay.push(delay);
+
+    dsp.lowpass.push(new SVF(pv.lowpassHz / upRate, pv.lowpassQ));
+    dsp.highpass.push(new SVF(pv.highpassHz / upRate, pv.highpassQ));
+  }
+
+  let sound = new Array(buf.length / upFold);
+  const processFunc
+    = menuitems.combSectionItems[pv.combSection] === "Active" ? process : (a, b, x) => x;
+  if (upFold == 2) {
+    let halfband = new multirate.HalfBandIIR();
+    for (let i = 0; i < sound.length; ++i) {
+      const hb0 = processFunc(pv, dsp, buf[2 * i]);
+      const hb1 = processFunc(pv, dsp, buf[2 * i + 1]);
+      sound[i] = halfband.process(hb0, hb1);
+    }
+  } else {
+    for (let i = 0; i < sound.length; ++i) sound[i] = processFunc(pv, dsp, buf[i]);
+  }
+
+  postMessage(sound);
+}
