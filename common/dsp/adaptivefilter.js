1a60b07701cbfdb9093e8309eeb150387e5235ec
Takamitsu Endo <ryukau@gmail.com>
2023-08-09 21:55:15 +0900

    Add AdaptiveNotchCPZ

--- common/dsp/adaptivefilter.js
+++ common/dsp/adaptivefilter.js
@@ -1,6 +1,8 @@
 // Copyright 2023 Takamitsu Endo
 // SPDX-License-Identifier: Apache-2.0
 
+import {clamp} from "../util.js"
+
 // Least mean square adaptive filter.
 // - https://en.wikipedia.org/wiki/Least_mean_squares_filter
 export class AdaptiveFilterLMS {
@@ -32,5 +34,56 @@ export class AdaptiveFilterLMS {
     for (let i = 0; i < this.src.length; ++i) this.fir[i] += k * this.src[i];
 
     return error;
+  }
+}
+
+// Constrained poles and zeros-based adaptive notch filters (CPZ-ANF) described in section
+// II of following paper.
+// - http://www.apsipa.org/proceedings/2018/pdfs/0001355.pdf
+//   - "DSP Implementation of Adaptive Notch Filters With Overflow Avoidance in
+//   Fixed-Point Arithmetic" by Satoru Ishibashi, Shunsuke Koshita, Masahide Abe and
+//   Masayuki Kawamata.
+//
+// The above paper refers to a 1985 paper linked below.
+// - https://www.ese.wustl.edu/~nehorai/paper/ieeeac85-2.pdf
+//   - A. Nehorai, "A minimal parameter adaptive notch filter with constrained poles and
+//   zeros," IEEE Trans. Acoust., Speech, Signal Process., vol. 33, no. 4, pp. 983–996,
+//   Aug. 1985.
+//
+// Gain response of this notch filter looks not good for audio but maybe there's some
+// application. Gain normalization is added for this implementation.
+class AdaptiveNotchCPZ {
+  constructor(sampleRate, initialGuessHz, narrownessOfNotch = 0.99) {
+    this.mu = 1 / sampleRate; // Step size, but there's no specification for this value.
+    this.rho = narrownessOfNotch;
+    this.a = -2 * Math.cos(2 * Math.pi * initialGuessFrequencyNormalized);
+
+    this.x1 = 0;
+    this.x2 = 0;
+    this.y1 = 0;
+    this.y2 = 0;
+  }
+
+  process(x0) {
+    const a1 = this.rho * this.a;
+    const a2 = this.rho * this.rho;
+
+    const y0 = x0 + this.a * this.x1 + this.x2 - a1 * this.y1 - a2 * this.y2;
+    const s0 = this.x1 * (1 - this.rho * this.y0);
+    this.a = clamp(this.a - 2 * y0 * s0 * this.mu, -2, 2);
+
+    this.x2 = this.x1;
+    this.x1 = x0;
+    this.y2 = this.y1;
+    this.y1 = y0;
+
+    // Normalize max output gain to 0 dB.
+    const zeroGain = 1 + a1 + a2;
+    const nyquistGain = 1 - a1 + a2;
+    const candidate = zeroGain >= nyquistGain ? zeroGain : nyquistGain;
+    const denom = Math.abs(candidate) <= Number.EPSILON
+      ? Math.sign(candidate) * Number.EPSILON
+      : candidate;
+    return y0 * (2 + this.a) / denom;
   }
 }
