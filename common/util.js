fe135fb8c24c2eef295f8a1245dc2309a7b23306
Takamitsu Endo <ryukau@gmail.com>
2026-08-06 17:55:00 +0900

    Change `midiPitchToFreq`

--- common/util.js
+++ common/util.js
@@ -11,7 +11,7 @@ export function lerp(v0, v1, t) { return v0 + t * (v1 - v0); }
 export function dbToAmp(dB) { return Math.pow(10, dB / 20); }
 export function ampToDB(amplitude) { return 20 * Math.log10(amplitude); }
 
-export function midiPitchToFreq(pitch) { return 440 * Math.pow(2, (pitch - 69) / 12); }
+export function midiPitchToFreq(pitch) { return 440 * Math.exp((pitch - 69) * (Math.LN2 / 12)); }
 export function freqToMidiPitch(freq) { return 69 + 12 * Math.log2(freq / 440); }
 
 export const syntonicCommaRatio = 81 / 80;
